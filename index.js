9b111381963e99a4366fe9052ee727e2095dce84
shownola <swasieko@gmail.com>
2020-02-05 11:05:14 -0700

    Implemented initial route using express framework

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
