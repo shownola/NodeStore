79cc4b0f5d87b4dfc9b50427004931ae44b48b0d
shownola <swasieko@gmail.com>
2020-02-05 13:57:56 -0700

    Implemented middleware to handle form data and parsing

--- index.js
+++ index.js
@@ -3,7 +3,38 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi');
+  res.send(`
+    <div>
+      <form action="" method="POST">
+        <input type="email" name="email" placeholder="email" />
+        <input type="password" name="password" placeholder="password" />
+        <input type="password" name="passwordConfirmation" placeholder="confirm password" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+    `);
+});
+
+const bodyParser = (req, res, next) => {
+  if(req.method === 'POST'){
+    req.on('data', data => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+      for (let pair of parsed){
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
+  res.send('Account created!!');
 });
 
 
