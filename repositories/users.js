0bc4b300676b7517445e639a543ed0f49d6979ca
shownola <swasieko@gmail.com>
2020-02-06 10:07:23 -0700

    Implemented update for users

--- repositories/users.js
+++ repositories/users.js
@@ -44,17 +44,30 @@ class UsersRepository {
     const filteredRecords = records.filter(record => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs){
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+
+    if(!record){
+      throw new Error(`Record ${id} not found`);
+    }
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  await repo.create({ email: 'test@test.com', password: 'password' });
-  const users = await repo.getAll();
+  // await repo.create({ email: 'test@test.com', password: 'password' });
+  // const users = await repo.getAll();
   // const user = await repo.getOne('12e8dd22');
-  console.log(user);
+  // console.log(user);
   // await repo.delete('12e8dd22');
+
+  await repo.update('9f8454cd', { password: 'mypassword' });
 };
 
 test();
