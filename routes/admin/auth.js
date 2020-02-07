28374bf5844d91f655a604175cc2d38ec6950419
shownola <swasieko@gmail.com>
2020-02-07 16:16:32 -0700

    Implemented custom validations

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -11,17 +12,42 @@ router.get('/signup', (req, res) => {
 });
 
 
-router.post('/signup', async (req, res) => {
+router.post('/signup',
+[
+  check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Must be a valid email')
+    .custom(async email => {
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+        throw new Error('Email in use');
+      }
+
+  }),
+  check('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Must be between 4 and 20 characters'),
+  check('passwordConfirmation')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Must be between 4 and 20 characters')
+    .custom((passwordConfirmation, { req }) => {
+      if (passwordConfirmation !== req.body.password) {
+        throw new Error('Passwords must match');
+      }
+    });
+
+],
+async (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
   const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email });
-  if(existingUser){
-    return res.send('Email is taken');
-  }
-  if(password !== passwordConfirmation){
-    return res.send('Passwords must match');
-  }
+
   const user = await usersRepo.create({ email, password });
-  // Store id of user inside the users cookie
+
   req.session.userId = user.id;
 
   res.send('Account created!!');
