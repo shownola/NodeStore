0b7bbfc6a68ac8ae5aedb576622dfbe059d5276a
shownola <swasieko@gmail.com>
2020-02-09 13:51:16 -0700

    Added products for UI

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express       = require('express');
 const bodyParser    = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter    = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cookieSession({
   keys: ['asdf234asdf']
 }));
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
 
 
