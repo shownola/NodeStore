64fdeed264627ec1c64b313d130cdc7726b4a289
shownola <swasieko@gmail.com>
2020-02-08 17:04:35 -0700

    Styled the new product page

--- repositories/repository.js
+++ repositories/repository.js
@@ -19,11 +19,15 @@ module.exports = class Repository {
     attrs.id = this.randomId();
 
     const records = await this.getAll();
-    records.push(attr);
+    records.push(attrs);
     await this.writeAll(records);
 
     return attrs;
   }
+
+
+
+
 
   async getAll(){
     // Open the file called this.filename
