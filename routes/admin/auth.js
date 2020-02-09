dda7b03391843c36c8b628c185f9208c7a6f2bcd
shownola <swasieko@gmail.com>
2020-02-08 17:32:50 -0700

    Refactor and add error handling middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -18,14 +18,10 @@ router.post('/signup',
   requireEmail,
   requirePassword,
   requirePasswsordConfirmation
-
 ],
+handleErrors(signupTemplate),
 async (req, res) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.send(signupTemplate({ req, errors }));
-  }
-  const { email, password, passwordConfirmation } = req.body;
+  const { email, password } = req.body;
   const user = await usersRepo.create({ email, password });
 
   req.session.userId = user.id;
@@ -46,14 +42,9 @@ router.post('/signin', [
   requireEmailExists,
   requireValidPasswordForUser
 ],
+  handleErrors(signinTemplate),
 async (req, res) => {
-  const errors = validationResult(req);
-
-  if(!errors.isEmpty()){
-    return res.send(signinTemplate({ errors }));
-  }
   const { email } = req.body;
-
   const user = await usersRepo.getOneBy({ email });
 
   req.session.userId = user.id;
