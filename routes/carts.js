e306c58e00cf805caedcfb5c0528d2c849de05ab
shownola <swasieko@gmail.com>
2020-02-10 09:36:10 -0700

    Implemented ability to add items to cart

--- routes/carts.js
+++ routes/carts.js
@@ -14,7 +14,16 @@ router.post('/cart/products', async (req, res) => {
   } else {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
-  console.log(cart);
+  const existingItem = cart.items.find(item => item.id === req.body.productId);
+  // increment quantity and save cart else add new product id to items array
+  if(existingItem){
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items
+  });
   res.send('Product added to cart');
 });
 
